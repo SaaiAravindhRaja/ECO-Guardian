a150d5859415ac7847a3cecaaa8da45bfeffae2c
ChaiAndCode <saaiaravindhraja@gmail.com>
2025-09-01 16:56:46 +0800

    edit app

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import { store } from '@/store';
 import { AppNavigator } from '@/navigation/AppNavigator';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { NetworkStatus } from '@/components/NetworkStatus';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <AuthProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
-      </AuthProvider>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <AuthProvider>
+          <NavigationContainer>
+            <StatusBar style="light" backgroundColor="#1B4332" />
+            <AppNavigator />
+            <NetworkStatus />
+          </NavigationContainer>
+        </AuthProvider>
+      </Provider>
+    </ErrorBoundary>
   );
 }
